a1b6d19b0d305d972908ee742ab0efcd2e3e734f
AlessioAbbati <alex_117@live.it>
2023-07-10 16:13:37 +0200

    mod aup and down function

--- database/migrations/2023_07_10_141013_update_projects_table.php
+++ database/migrations/2023_07_10_141013_update_projects_table.php
@@ -14,7 +14,13 @@ return new class extends Migration
     public function up()
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            // creo la colonna della chiave esterna
+
+            $table->unsignedBigInteger('type_id');
+
+            // definire la colonna come chiave esterna
+
+            $table->foreign('type_id')->references('id')->on('types');
         });
     }
 
@@ -26,7 +32,13 @@ return new class extends Migration
     public function down()
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+             // elimino la chiave esterna
+
+             $table->dropForeign('projects_type_id_foreign');
+
+             // elimino la colonna
+ 
+             $table->dropColumn('type_id');
         });
     }
 };
