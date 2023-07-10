b6356ec651483723482f488b5391532128703a25
AlessioAbbati <alex_117@live.it>
2023-07-10 17:11:20 +0200

    populate db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            
+            TypesTableSeeder::class,
             ProjectsTableSeeder::class,
         ]);
     }
